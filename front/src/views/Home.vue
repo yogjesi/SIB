9d8d37cd81188754022b2d13c241fd36c262bea5
rnfmal0301 <rnfmal0301@naver.com>
2021-12-30 11:07:20 +0900

    suhyun_redirect_bible 최종

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -2,34 +2,26 @@
   <div class="home">
     <h2 v-if="currentUser.authority > 1 | currentUser.is_superuser">현재 청년부 총 잔액은 <span class="display-3">{{lastBalance}}</span>원 입니다.</h2>
     <b-button @click="randomBible">클릭</b-button>
-    <p>{{bible.index}} {{bible.content}}</p>
+    <p>{{bibleList}} {{bibleList.contet}}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import _ from 'lodash'
+// import _ from 'lodash'
 import { mapState } from 'vuex'
 
 export default {
   name: 'Home',
-  data:function(){
-    return{
-      bible:[],
-    }
-  },
   methods:{
     randomBible: function(){
-      this.bible = _.sampleSize(this.bibleList,1)[0]
+      this.$store.dispatch('bibleList')
     }
   },
   computed:{
     ...mapState([
       'bibleList','lastBalance','currentUser'
-    ])
-  },
-  created:function(){
-    this.bible = _.sampleSize(this.bibleList,1)[0]
+    ]),
   },
 }
 </script>
